71f5fefd1090be53406c7ba5b5764ed1f00ba34a
abiraltamang <abiral.tamang816@gmail.com>
2023-12-06 07:50:22 +0545

    fetchMyNFTs function

--- src/pages/MyNFTs.tsx
+++ src/pages/MyNFTs.tsx
@@ -27,42 +27,83 @@ const MyNFTs = () => {
   }, [walletProvider]);
 
   async function loadNFTs() {
-    if (!walletProvider) {
-      throw Error("Wallet provider is undefined");
+    try {
+      if (!walletProvider) {
+        throw Error("Wallet provider is undefined");
+      }
+      const provider = new BrowserProvider(walletProvider);
+      const signer = await provider.getSigner();
+
+      console.log("SIgner provided----", signer);
+
+      const marketplaceContract = new ethers.Contract(
+        marketplaceAddress,
+        NFTMarketplace.abi,
+        signer
+      );
+      const data = await marketplaceContract.fetchMyNFTs();
+
+      // Fetch additional details for each item
+      const items = await Promise.all(
+        data.map(async (item) => {
+          const tokenUri = await marketplaceContract.tokenURI(item.tokenId);
+          console.log(tokenUri);
+          let meta;
+          if (tokenUri.startsWith("ipfs://")) {
+            // Handle IPFS protocol separately
+            // You may need to use an IPFS library or gateway to fetch the data
+            // For simplicity, this example assumes the IPFS link is just an HTTP link
+            const ipfsLink = tokenUri.replace(
+              "ipfs://",
+              "https://ipfs.io/ipfs/"
+            );
+            meta = await axios.get(ipfsLink);
+          } else {
+            // Fetch metadata using the regular HTTP protocol
+            meta = await axios.get(tokenUri);
+          }
+
+          // Format the item details
+          const formattedItem = {
+            price: ethers.formatUnits(item.price.toString(), "ether"),
+            tokenId: item.tokenId,
+            seller: item.seller,
+            owner: item.owner,
+            image: meta.data.image,
+            name: meta.data.name,
+            description: meta.data.description,
+            tokenUri,
+          };
+
+          return formattedItem;
+        })
+      );
+
+      // const items = await Promise.all(
+      //   data.map(async (i) => {
+      //     const tokenURI = await marketplaceContract.tokenURI(i.tokenId);
+      //     const meta = await axios.get(tokenURI);
+      //     const price = ethers.formatUnits(i.price.toString(), "ether");
+      //     const item = {
+      //       price,
+      //       tokenId: i.tokenId,
+      //       seller: i.seller,
+      //       owner: i.owner,
+      //       image: meta.data.image,
+      //       name: meta.data.name,
+      //       description: meta.data.description,
+      //       tokenURI,
+      //     };
+      //     return item;
+      //   })
+      // );
+      setNfts(items);
+      console.log(items);
+
+      setLoadingState("loaded");
+    } catch (error) {
+      console.log("Error loading my NFTs"), error;
     }
-    const provider = new BrowserProvider(walletProvider);
-    const signer = await provider.getSigner();
-
-    console.log("SIgner provided----", signer);
-
-    const marketplaceContract = new ethers.Contract(
-      marketplaceAddress,
-      NFTMarketplace.abi,
-      provider
-    );
-    const data = await marketplaceContract.fetchMyNFTs();
-
-    const items = await Promise.all(
-      data.map(async (i) => {
-        const tokenURI = await marketplaceContract.tokenURI(i.tokenId);
-        console.log("Token URI:", tokenURI);
-        const meta = await axios.get(tokenURI);
-        console.log("Metadata:", meta.data);
-        const price = ethers.formatUnits(i.price.toString(), "ether");
-        const item = {
-          price,
-          tokenId: i.tokenId.toNumber(),
-          seller: i.seller,
-          owner: i.owner,
-          image: meta.data.image,
-          tokenURI,
-        };
-        return item;
-      })
-    );
-    setNfts(items);
-    console.log(items);
-    setLoadingState("loaded");
   }
   function listNFT(nft) {
     console.log("nft:", nft);
@@ -79,14 +120,14 @@ const MyNFTs = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {nfts.map((nft, i) => (
             <div key={i} className="border shadow rounded-xl overflow-hidden">
-              <img src={nft.image} className="rounded" />
+              {/* <img src={nft.image} className="rounded" /> */}
               <div className="p-4 bg-black">
                 <p className="text-2xl font-bold text-white">
                   Price - {nft.price} Eth
                 </p>
                 <button
                   className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  onClick={() => listNFT(nft)}
+                  // onClick={() => listNFT(nft)}
                 >
                   List
                 </button>
