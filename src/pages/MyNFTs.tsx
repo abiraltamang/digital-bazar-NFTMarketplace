db5612f8aaa963b926d5a0851ea152901a48ca1f
abiraltamang <abiral.tamang816@gmail.com>
2023-12-07 00:26:25 +0545

    tested

--- src/pages/MyNFTs.tsx
+++ src/pages/MyNFTs.tsx
@@ -7,6 +7,8 @@ import { ethers } from "ethers";
 import { useWeb3ModalProvider } from "@web3modal/ethers/react";
 import { NFT } from "./Home";
 import { BrowserProvider } from "ethers";
+import Button from "../components/common/Button/Button.js";
+import { MarketItem } from "./Home";
 
 //@ts-expect-error config
 import { marketplaceAddress } from "../../config.js";
@@ -34,7 +36,7 @@ const MyNFTs = () => {
       const provider = new BrowserProvider(walletProvider);
       const signer = await provider.getSigner();
 
-      console.log("SIgner provided----", signer);
+      console.log("SIgner provided----", provider);
 
       const marketplaceContract = new ethers.Contract(
         marketplaceAddress,
@@ -45,7 +47,7 @@ const MyNFTs = () => {
 
       // Fetch additional details for each item
       const items = await Promise.all(
-        data.map(async (item) => {
+        data.map(async (item: MarketItem) => {
           const tokenUri = await marketplaceContract.tokenURI(item.tokenId);
           console.log(tokenUri);
           let meta;
@@ -78,64 +80,53 @@ const MyNFTs = () => {
           return formattedItem;
         })
       );
-
-      // const items = await Promise.all(
-      //   data.map(async (i) => {
-      //     const tokenURI = await marketplaceContract.tokenURI(i.tokenId);
-      //     const meta = await axios.get(tokenURI);
-      //     const price = ethers.formatUnits(i.price.toString(), "ether");
-      //     const item = {
-      //       price,
-      //       tokenId: i.tokenId,
-      //       seller: i.seller,
-      //       owner: i.owner,
-      //       image: meta.data.image,
-      //       name: meta.data.name,
-      //       description: meta.data.description,
-      //       tokenURI,
-      //     };
-      //     return item;
-      //   })
-      // );
       setNfts(items);
       console.log(items);
-
       setLoadingState("loaded");
     } catch (error) {
       console.log("Error loading my NFTs"), error;
     }
   }
-  function listNFT(nft) {
+  function listNFT(nft: NFT) {
     console.log("nft:", nft);
     navigate(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`);
   }
-  //   if (loadingState === "loaded" && !nfts.length)
-  //     return <h1 className="py-10 px-20 text-3xl">No NFTs owned</h1>;
   return (
     <SectionWrapper>
       <Text className="text-center text-black/60 pb-8" title weight="semibold">
         My NFTs
       </Text>
-      <div className="p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {nfts.map((nft, i) => (
-            <div key={i} className="border shadow rounded-xl overflow-hidden">
-              {/* <img src={nft.image} className="rounded" /> */}
-              <div className="p-4 bg-black">
-                <p className="text-2xl font-bold text-white">
-                  Price - {nft.price} Eth
-                </p>
-                <button
-                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  // onClick={() => listNFT(nft)}
-                >
-                  List
-                </button>
+      {loadingState === "loaded" && !nfts.length ? (
+        <>
+          <Text className="text-center">No NFts owned</Text>
+          <div className="flex justify-center mt-6">
+            <Button primary rounded>
+              Explore NFTs
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div className="p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {nfts.map((nft, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} className="rounded" />
+                <div className="p-4 bg-black">
+                  <p className="text-2xl font-bold text-white">
+                    Price - {nft.price} Eth
+                  </p>
+                  <button
+                    className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                    onClick={() => listNFT(nft)}
+                  >
+                    List
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </SectionWrapper>
   );
 };
