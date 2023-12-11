d349fa66baafd1c89dc0f270ae336391abf31cf3
abiraltamang <abiral.tamang816@gmail.com>
2023-12-11 22:41:54 +0545

    added progress symbol when loading

--- src/components/pages/ProductPage/ProductDetail.tsx
+++ src/components/pages/ProductPage/ProductDetail.tsx
@@ -3,6 +3,8 @@ import Button from "../../common/Button/Button";
 import { NFT } from "../../../pages/Home";
 import { extractCIDFromImage } from "../../../utils/index";
 import { shortenAddress } from "../../../utils/index";
+import { useState } from "react";
+import { Audio } from "react-loader-spinner";
 
 interface ProductDetailProps {
   nft: NFT;
@@ -14,8 +16,19 @@ const ProductDetail: React.FC<ProductDetailProps> = ({
   buyNFT,
 }: ProductDetailProps) => {
   const { image, name, description, price, owner } = nft;
+  const [loading, setLoading] = useState(false);
   const CID = extractCIDFromImage(image);
 
+  const handleBuyButton = async () => {
+    setLoading(true);
+    try {
+      await buyNFT(nft);
+    } catch (error) {
+      console.error("Error while buying NFT:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="grid py-24 px-40 grid-cols-2 gap-6 rounded-xl">
       <div className="w-full h-ful hover:shadow-lg hover:shadow-gray-700 tansition-all ease-in-out duration-300 hover:cursor-pointer">
@@ -41,8 +54,15 @@ const ProductDetail: React.FC<ProductDetailProps> = ({
         <Button secondary className="bg-primary/80 text-black">
           {price} ETH
         </Button>
-        <Button primary onClick={() => buyNFT(nft)}>
-          Buy Now
+        <Button primary onClick={handleBuyButton} disabled={loading}>
+          {loading ? (
+            <div className="flex items-center justify-center gap-5">
+              <Audio color="white" height="20" width="20" />
+              <p>Buy in progress</p>
+            </div>
+          ) : (
+            "Buy Now"
+          )}
         </Button>
       </div>
     </div>
